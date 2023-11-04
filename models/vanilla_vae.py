0b8fa0f6261ee869f74e62fe32d650a0cc433511
Bhavya Kohli <bhavyakohli2024@gmail.com>
2023-11-05 00:39:17 +0530

    modified vanilla_vae to be able to take generic input shapes

--- models/vanilla_vae.py
+++ models/vanilla_vae.py
@@ -1,4 +1,6 @@
 import torch
+import numpy as np
+
 from models import BaseVAE
 from torch import nn
 from torch.nn import functional as F
@@ -6,8 +8,6 @@ from .types_ import *
 
 
 class VanillaVAE(BaseVAE):
-
-
     def __init__(self,
                  in_channels: int,
                  latent_dim: int,
@@ -16,12 +16,21 @@ class VanillaVAE(BaseVAE):
         super(VanillaVAE, self).__init__()
 
         self.latent_dim = latent_dim
+        if hidden_dims is None:
+            self.hidden_dims = [32, 64, 128, 256, 512]
+        else:
+            self.hidden_dims = hidden_dims
+        self.in_channels = in_channels
+
+        self.built = False
+    
+    def build(self, dummy_batch, device):
+        hidden_dims = self.hidden_dims
+        input_shape = dummy_batch.shape
 
         modules = []
-        if hidden_dims is None:
-            hidden_dims = [32, 64, 128, 256, 512]
-
         # Build Encoder
+        in_channels = self.in_channels
         for h_dim in hidden_dims:
             modules.append(
                 nn.Sequential(
@@ -33,46 +42,56 @@ class VanillaVAE(BaseVAE):
             in_channels = h_dim
 
         self.encoder = nn.Sequential(*modules)
-        self.fc_mu = nn.Linear(hidden_dims[-1]*4, latent_dim)
-        self.fc_var = nn.Linear(hidden_dims[-1]*4, latent_dim)
+        dummy_encoder_result = self.encoder(dummy_batch)
+        self.encoder_result_shape = dummy_encoder_result.shape[1:]
 
+        self.fc_mu = nn.Linear(np.prod(self.encoder_result_shape), self.latent_dim)
+        self.fc_var = nn.Linear(np.prod(self.encoder_result_shape), self.latent_dim)
+        dummy_mu = self.fc_mu(torch.flatten(dummy_encoder_result, start_dim=1))
 
         # Build Decoder
         modules = []
+        self.decoder_input = nn.Linear(self.latent_dim, np.prod(self.encoder_result_shape))
 
-        self.decoder_input = nn.Linear(latent_dim, hidden_dims[-1] * 4)
+        hidden_dims_r = hidden_dims[::-1]
 
-        hidden_dims.reverse()
-
-        for i in range(len(hidden_dims) - 1):
+        for i in range(len(hidden_dims_r) - 1):
             modules.append(
                 nn.Sequential(
-                    nn.ConvTranspose2d(hidden_dims[i],
-                                       hidden_dims[i + 1],
+                    nn.ConvTranspose2d(hidden_dims_r[i],
+                                       hidden_dims_r[i + 1],
                                        kernel_size=3,
                                        stride = 2,
                                        padding=1,
                                        output_padding=1),
-                    nn.BatchNorm2d(hidden_dims[i + 1]),
+                    nn.BatchNorm2d(hidden_dims_r[i + 1]),
                     nn.LeakyReLU())
             )
-
-
-
+        
         self.decoder = nn.Sequential(*modules)
-
+        
         self.final_layer = nn.Sequential(
-                            nn.ConvTranspose2d(hidden_dims[-1],
-                                               hidden_dims[-1],
+                            nn.ConvTranspose2d(hidden_dims_r[-1],
+                                               hidden_dims_r[-1],
                                                kernel_size=3,
                                                stride=2,
                                                padding=1,
                                                output_padding=1),
-                            nn.BatchNorm2d(hidden_dims[-1]),
-                            nn.LeakyReLU(),
-                            nn.Conv2d(hidden_dims[-1], out_channels= 3,
-                                      kernel_size= 3, padding= 1),
-                            nn.Tanh())
+                            nn.BatchNorm2d(hidden_dims_r[-1]),
+                            nn.LeakyReLU()
+        )
+
+        dummy_decoder_result = self.final_layer(self.decoder(self.decoder_input(dummy_mu).view(-1, *self.encoder_result_shape)))
+        decoder_result_shape = dummy_decoder_result.shape
+
+        required_dim = input_shape[-1]
+        current_dim = decoder_result_shape[-1]
+        kernel = int(-((required_dim - 1) * 1 + 1 - 2*1 - current_dim) / 1 + 1)                     
+
+        self.output_layer = nn.Conv2d(hidden_dims_r[-1], out_channels=self.in_channels,
+                                      kernel_size=kernel, padding=1)
+        self.built = True
+        self = self.to(device)
 
     def encode(self, input: Tensor) -> List[Tensor]:
         """
@@ -81,6 +100,7 @@ class VanillaVAE(BaseVAE):
         :param input: (Tensor) Input tensor to encoder [N x C x H x W]
         :return: (Tensor) List of latent codes
         """
+        if not self.built: raise AttributeError("build model first using model.build") 
         result = self.encoder(input)
         result = torch.flatten(result, start_dim=1)
 
@@ -98,10 +118,12 @@ class VanillaVAE(BaseVAE):
         :param z: (Tensor) [B x D]
         :return: (Tensor) [B x C x H x W]
         """
+        if not self.built: raise AttributeError("build model first using model.build") 
         result = self.decoder_input(z)
-        result = result.view(-1, 512, 2, 2)
+        result = result.view(-1, *self.encoder_result_shape)
         result = self.decoder(result)
         result = self.final_layer(result)
+        result = self.output_layer(result)
         return result
 
     def reparameterize(self, mu: Tensor, logvar: Tensor) -> Tensor:
@@ -117,6 +139,7 @@ class VanillaVAE(BaseVAE):
         return eps * std + mu
 
     def forward(self, input: Tensor, **kwargs) -> List[Tensor]:
+        if not self.built: raise AttributeError("build model first using model.build") 
         mu, log_var = self.encode(input)
         z = self.reparameterize(mu, log_var)
         return  [self.decode(z), input, mu, log_var]
@@ -131,13 +154,16 @@ class VanillaVAE(BaseVAE):
         :param kwargs:
         :return:
         """
+        if not self.built: raise AttributeError("build model first using model.build") 
+
         recons = args[0]
         input = args[1]
         mu = args[2]
         log_var = args[3]
 
         kld_weight = kwargs['M_N'] # Account for the minibatch samples from the dataset
-        recons_loss =F.mse_loss(recons, input)
+        recons_loss = F.mse_loss(recons, input)
+        # recons_loss = F.mse_loss(gops.gumbel_sinkhorn(recons[:,0])[:,None], input)
 
 
         kld_loss = torch.mean(-0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim = 1), dim = 0)
